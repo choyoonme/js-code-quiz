f78e96cb2778bc7132d8e2cd9253aea7b33615de
Jen Cho <99620655+choyoonme@users.noreply.github.com>
2022-03-26 19:22:18 -0400

    add variable to dos

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 /*Create variables and target in DOM:
-timer
-score
-question; use textContent to cycle through 5 different questions
-button id; use textContent to cycle through each set of 4 answers
-button to start game
-button to submit initials
+
+-score
+-timer
+-button to start game
+-question; use textContent to cycle through 5 different questions
+-answer button id; use textContent to cycle through each set of 4 answers
+-button to submit initials
 
 /*Functions:
 
-cycle through 5 questions with a set of 4 answers for each question
-show correct or incorrect using conditional statements and data-state: "hidden"
-start timer/game and run for duration of quiz using setInterval
-stop timer when all 5 questions are answered using clearInterval
-store the count of correctly answered questions in localStorage    
+-cycle through 5 questions with a set of 4 answers for each question
+-show correct or incorrect using conditional statements and data-state: "hidden"
+-start timer/game and run for duration of quiz using setInterval
+-stop timer when all 5 questions are answered using clearInterval
+-store the count of correctly answered questions and submit initials in localStorage    
 */
