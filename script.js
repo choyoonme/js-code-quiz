edc83a2f857c740adcc7804b67a9632bfedd1cd7
Jen Cho <99620655+choyoonme@users.noreply.github.com>
2022-03-26 19:16:15 -0400

    add js TODOs and buttons in html

---
+++ script.js
@@ -0,0 +1,16 @@
+/*Create variables and target in DOM:
+timer
+score
+question; use textContent to cycle through 5 different questions
+button id; use textContent to cycle through each set of 4 answers
+button to start game
+button to submit initials
+
+/*Functions:
+
+cycle through 5 questions with a set of 4 answers for each question
+show correct or incorrect using conditional statements and data-state: "hidden"
+start timer/game and run for duration of quiz using setInterval
+stop timer when all 5 questions are answered using clearInterval
+store the count of correctly answered questions in localStorage    
+*/
